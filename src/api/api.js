2cb83b436d7b9d3d7dc7cae2aacfe17bc418218d
zongxiaoliang <996456708@qq.com>
2022-06-10 18:04:47 +0800

    修改请求配置

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Vue.prototype.qs = qs
 
 if (curMode == 'development') {
-    axios.defaults.baseURL = 'http://127.0.0.1:4523/mock/908535/api';
+    axios.defaults.baseURL = 'http://127.0.0.1:4523/mock/908535/';
 } else if (curMode == 'production') {
     axios.defaults.baseURL = window.location.href + "api";
 }
@@ -24,7 +24,7 @@ async function httppost(url, para) {
 
 //获取设备列表
 async function GetDeviceList() {
-    let res = await httpget('base/deviceList')
+    let res = await httpget('api/base/deviceList')
     return res;
 }
 
@@ -33,43 +33,43 @@ async function GetDeviceData(params) {
     let pms = {
         dev: params
     }
-    let res = await httppost('base/realtime', pms)
+    let res = await httppost('api/base/realtime', pms)
     return res
 }
 
 //获取设备历史台账数据
 async function GetDeviceHistoryData(params) {
-    let res = await httppost('base/record', params)
+    let res = await httppost('api/base/record', params)
     return res;
 }
 
 //获取设备告警事件数据
 async function GetAlarmEvent(params) {
-    let res = await httppost('base/alarm', params)
+    let res = await httppost('api/base/alarm', params)
     return res;
 }
 
 //获取设备故障波形数据
 async function GetErrorState(params) {
-    let res = await httppost('base/faultWaveform', params)
+    let res = await httppost('api/base/faultWaveform', params)
     return res;
 }
 
 //获取设备参数
 async function GetDevParams(params) {
-    let res = await httppost('base/getConfig', params)
+    let res = await httppost('api/base/getConfig', params)
     return res;
 }
 
 //上传设备参数
 async function SetDevParams(params) {
-    let res = await httppost('base/setConfig', params)
+    let res = await httppost('api/base/setConfig', params)
     return res;
 }
 
 //登录
 async function LoginParams(params) {
-    let res = await httppost('user/login', params)
+    let res = await httppost('api/user/login', params)
     return res;
 }
 
