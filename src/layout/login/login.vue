76cb3b8a688a58347b7d26eaac129905c5ec2a49
zongxiaoliang <996456708@qq.com>
2022-06-22 14:09:44 +0800

    云端模拟数据

--- src/layout/login/login.vue
+++ src/layout/login/login.vue
@@ -2,7 +2,7 @@
     <div class="login">
         <div class="header">
             <img src="../../assets/img/login-logo.png" alt="" />
-            <span class="title">低压台区漏电管家 自动化构建测试</span>
+            <span class="title">低压台区漏电管家 云端mock数据</span>
         </div>
         <div class="body">
             <div class="left"></div>
